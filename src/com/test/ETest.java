948d218a5b1f512506c24f77333c3923445d27a5
Kevin <zongjie.xia@gmail.com>
2015-08-11 13:27:12 +0800

    create

---
+++ src/com/test/ETest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author Administrator
+ *
+ */
+public class ETest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
